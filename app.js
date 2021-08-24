a46cf89389ea8d078a145d014bb321bb49c02d99
chia hung <a0926223478@hotmail.com>
2021-08-24 17:07:56 +0800

    feat:Install passport and passport-local

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const db = require('./models')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({extended: true}))
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -25,5 +28,5 @@ app.listen(port, () => {
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 // const router =require('./routes')
 // router(app)
-require('./routes')(app)
+require('./routes')(app, passport)
 module.exports = app
