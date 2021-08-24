c39ebbb5de35f883f91487e5c3c016c3d44785c1
chia hung <a0926223478@hotmail.com>
2021-08-24 15:36:56 +0800

    feat:add function:Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -8,7 +10,14 @@ app.use(express.urlencoded({extended: true}))
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false}))
+app.use(flash())
 
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_message')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
