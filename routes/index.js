fce2f74e805fa1b2c384522f33926af7c417360b
chia hung <a0926223478@hotmail.com>
2021-08-24 18:29:45 +0800

    feat: add isAdmin to users table & authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,28 @@ const userController = require('../controllers/userController.js')
 
 
 module.exports = (app, passport) => {
-  //使用者頁面
-  app.get('/', (req, res) => {
-    res.redirect('/restaurants')
-  })
+  //帳號驗證
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/restaurants', restController.getRestaurants)
+  //使用者頁面
+  app.get('/', authenticated, (req, res) => { res.redirect('/restaurants') })
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   //管理者頁面
-  app.get('/admin', (req, res) => {
-    res.redirect('/admin/restaurants')
-  })
-
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => { res.redirect('/admin/restaurants') })
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   //註冊頁面
   app.get('/signup', userController.signUpPage)
@@ -24,5 +33,6 @@ module.exports = (app, passport) => {
   
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
   app.get('/logout', userController.logout)
 }
