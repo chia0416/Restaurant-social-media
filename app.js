93cd8ec96c1a3267e00bfea86b9e45f0b8212251
chia hung <a0926223478@hotmail.com>
2021-08-25 19:36:55 +0800

    Listen to process.env.PORT & define Procfile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const session = require('express-session')
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
 
