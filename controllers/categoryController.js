24ed1f95f3d9670c6814a236db91e6be3abf53ab
chiahung <a0926223478@hotmail.com>
2021-09-01 18:18:27 +0800

    feat: Add admin categories edit and put feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,30 +2,57 @@ const db = require('../models')
 const Category = db.Category
 
 let categoryController = {
-    //瀏覽目錄
-    getCategories: (req, res) => {
-        return Category.findAll({
-            raw: true,
-            nest: true
-        }).then(categories => {
-            return res.render('admin/categories', { categories: categories })
-        })
-    },
-    
-    //新增分類名稱
-    postCategory: (req, res) => {
-        if(!req.body.name) {
-            req.flash('error_message', '請輸入分類名稱')
-            return res.redirect('back')
-        } else {
-            return Category.create({
-                name: req.body.name
+  
+  //瀏覽目錄
+  getCategories: (req, res) => {
+    return Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      if (req.params.id) {
+        Category.findByPk(req.params.id)
+          .then((category) => {
+            return res.render('admin/categories', { 
+              categories: categories, 
+              category: category.toJSON() 
             })
-            .then(() => {
-                res.redirect('/admin/categories')
-            })
-        }
+          })
+      } else {
+        return res.render('admin/categories', { categories: categories })
+      }
+    })
+  },
+  
+  //新增分類名稱
+  postCategory: (req, res) => {
+    if(!req.body.name) {
+      req.flash('error_message', '請輸入分類名稱')
+      return res.redirect('back')
+    } else {
+      return Category.create({
+          name: req.body.name
+      })
+      .then(() => {
+          res.redirect('/admin/categories')
+      })
     }
+  },
+
+  //編輯分類名稱
+  putCategory: (req, res) => {
+    if(!req.body.name) {
+      req.flash('error_message', '請輸入分類名稱')
+      return res.redirect('back')
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then(() =>{
+              res.redirect('/admin/categories')
+            })
+        })
+    }
+  }
 }
 
 module.exports = categoryController
--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,9 @@ module.exports = (app, passport) => {
   //CURD
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
-  
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+
   // 註冊頁面 => userController
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
