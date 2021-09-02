dc4e1e3ae89a981f0ea48eff0f28b668edfbc745
chiahung <a0926223478@hotmail.com>
2021-09-02 16:14:26 +0800

    feat: Add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,23 @@
+const db  = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({
+      include: Category
+    })
+      .then(restaurants => {
+        //將restaurants 內部資料展開一一傳入data
+        const data = restaurants.map (r => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50),
+          categoryName: r.Category.name  //如果類別為空則跳BUG
+        }))
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
   }
 }
 module.exports = restController
