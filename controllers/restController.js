40b7c0b249289ab39996c19ff8fa65922de21caa
chiahung <a0926223478@hotmail.com>
2021-09-04 15:35:36 +0800

    feat:Add add/remove favorite button to restaurant & restaurants page

--- controllers/restController.js
+++ controllers/restController.js
@@ -38,7 +38,8 @@ const restController = {
       const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
-        categoryName: r.dataValues.Category.name // 如果類別為空則跳BUG
+        categoryName: r.dataValues.Category.name, 
+        isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
       }))
       Category.findAll({
         raw: true,
@@ -61,12 +62,15 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
+        { model: User, as: 'FavoritedUsers' },
         { model: Comment, include: [User] }
       ]
     }).then(restaurant => {
+      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
       restaurant.increment('viewCounts')
       return res.render('restaurant', {
-        restaurant: restaurant.toJSON()
+        restaurant: restaurant.toJSON(),
+        isFavorited
       })
     })
   },
