a69b02c0e3c3db2fccaab3e95c0b3d58eddca110
chiahung <a0926223478@hotmail.com>
2021-09-06 17:24:18 +0800

    refactor: refactor router

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.listen(port, () => {
 })
 
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-// const router =require('./routes')
+// const router = require('./routes')
 // router(app)
-require('./routes')(app, passport)
+require('./routes')(app)
 module.exports = app
